dddf9e75d29a47ff89af5abd1bf7407042a16840
yuzuneko <13816640914@163.com>
2018-06-27 23:54:00 +0800

    33

--- emoney_web_beta2/app2/urls.py
+++ emoney_web_beta2/app2/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url,include
 from . import views
 urlpatterns = [
     
-    url('^page1/index',views.index),
-    url('^page1/show',views.show),
+    url('^page1/index/',views.index),
+    url('^page1/show/',views.show),
+    url('^page1/show2/',views.show2),
 ]
--- emoney_web_beta2/app2/views.py
+++ emoney_web_beta2/app2/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+import json
+import time
 # Create your views here.
+from emoney.settings import logger
+
+def time_sort(l):
+
+	l2=[]
+	l1=[]
+	for i in l:
+		if '.png' in i :
+			l1.append(i)
+			l2.append(time.mktime(time.strptime(i[-14:-4],'%Y-%m-%d')))
+	# print(l2)
+	l=[]
+	for i in range(len(l1)):
+		l.append((l1[i],l2[i]))
+	# print(l)
+	l.sort(key=lambda x:x[1],reverse=True)
+	# print(l)
+	l2=[]
+	for i in l:
+		l2.append(i[0])
+	# print(l2)
+	return l2
+
+
+
 def index(request):
 	return HttpResponse('nihao')
 def show(request):
 
-	return render(request,'show.html')
+	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	# logger.info(BASE_DIR)
+	filelist=os.listdir(BASE_DIR+'//app2//static//data_img')
+	filelist=time_sort(filelist)
+	filelist=json.dumps(filelist)
+	context={
+		'filelist':filelist,
+	}
+	return render(request,'show.html',context=context)
+
+def show2(request):
+
+	return render(request,'echart1.html')
